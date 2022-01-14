3a7bd8ed2e845537ebd535a72e8a1c40cefc0d9d
Govind Maheshwari <45732601+govindmaheshwari2@users.noreply.github.com>
2022-01-14 15:13:57 +0530

    Update HmsSdkManager.dart

--- lib/setup/HmsSdkManager.dart
+++ lib/setup/HmsSdkManager.dart
@@ -1,12 +1,7 @@
+//Project imports
+
 import 'package:zoom/setup/hms_sdk_interactor.dart';
 
 class HmsSdkManager {
   static HMSSDKInteractor? hmsSdkInteractor = HMSSDKInteractor();
-
-  HMSSDKInteractor createInstance() {
-    if (HmsSdkManager.hmsSdkInteractor == null) {
-      hmsSdkInteractor = HMSSDKInteractor();
-    }
-    return hmsSdkInteractor!;
-  }
 }
