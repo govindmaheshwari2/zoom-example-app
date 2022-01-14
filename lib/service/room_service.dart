472c4571055127ae96a71f7b2e0b4cb4f9b6d9d5
Govind Maheshwari <45732601+govindmaheshwari2@users.noreply.github.com>
2022-01-14 15:20:59 +0530

    Update room_service.dart

--- lib/service/room_service.dart
+++ lib/service/room_service.dart
@@ -1,18 +1,19 @@
+//dart imports
 import 'dart:convert';
+
+//Package imports
 import 'package:http/http.dart' as http;
 
 class RoomService {
   Future<List<String?>?> getToken(
       {required String user, required String room}) async {
     List<String?> codeAndDomain = getCode(room) ?? [];
-    print(codeAndDomain);
     if (codeAndDomain.length == 0) {
       return null;
     }
     Uri endPoint = codeAndDomain[2] == "true"
         ? Uri.parse("https://prod-in.100ms.live/hmsapi/get-token")
         : Uri.parse("https://qa-in.100ms.live/hmsapi/get-token");
-
     http.Response response = await http.post(endPoint, body: {
       'code': (codeAndDomain[1] ?? "").trim(),
       'user_id': user,
@@ -21,13 +22,12 @@ class RoomService {
     });
 
     var body = json.decode(response.body);
-    print(body);
     return [body['token'], codeAndDomain[2]!.trim()];
   }
 
-  List<String?>? getCode(String? roomUrl) {
-    String? url = roomUrl;
-    if (url == null) return [];
+  List<String?>? getCode(String roomUrl) {
+    String url = roomUrl;
+    if (url == "") return [];
     url = url.trim();
     bool isProdM = url.contains(".app.100ms.live/meeting/");
     bool isProdP = url.contains(".app.100ms.live/preview/");
@@ -45,14 +45,12 @@ class RoomService {
           : url.split(".app.100ms.live/preview/");
       code = codeAndDomain[1];
       subDomain = codeAndDomain[0].split("https://")[1] + ".app.100ms.live";
-      print("$subDomain $code");
     } else if (isQaM || isQaP) {
       codeAndDomain = isQaM
           ? url.split(".qa-app.100ms.live/meeting/")
           : url.split(".qa-app.100ms.live/preview/");
       code = codeAndDomain[1];
       subDomain = codeAndDomain[0].split("https://")[1] + ".qa-app.100ms.live";
-      print("$subDomain $code");
     }
     return [subDomain, code, isProdM || isProdP ? "true" : "false"];
   }
