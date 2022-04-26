cff1918bfc2160a6c8f92973196dde8d97724651
Govind Maheshwari <45732601+govindmaheshwari2@users.noreply.github.com>
2022-04-27 03:18:49 +0530

    ^0.7.0

--- lib/meeting.dart
+++ lib/meeting.dart
@@ -214,7 +214,7 @@ class _MeetingState extends State<Meeting> with WidgetsBindingObserver {
         // return Observer(builder: (context) {
         //   return videoTile(
         //       tracks[start + index],
-        //       !(tracks[start + index].track?.peer?.isLocal ?? false
+        //       !(tracks[start + index].peer.isLocal
         //           ? !_meetingStore.isVideoOn
         //           : (trackUpdate[tracks[start + index].peerId]) ==
         //               HMSTrackUpdate.trackMuted),
